49fc4e9a981f253cfe41ed331160c1d5ccc4859d
Diego Cruz <diegotombos@gmail.com>
2023-05-05 06:38:40 -0300

    corrige texto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World');
+    res.send('<h1>Hello World</h1>');
 })
 
 app.listen(3000, () => console.log("Servidor iniciado..."))
